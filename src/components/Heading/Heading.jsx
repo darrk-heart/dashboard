5200f1f8c088b5b8810b68d4ba94a97384a93939
darrk-heart <prospaul999@gmail.com>
2024-03-21 15:25:15 +0000

    heading okay

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaBars } from "react-icons/fa";
 import styles from "./Heading.module.css";
 import { MdApps } from "react-icons/md";
+import background from "../../assets/background.jpg";
 
 function Heading() {
   return (
@@ -9,12 +10,24 @@ function Heading() {
       <div className={styles.heading}>
         <div className={styles.googleHeading}>
           <FaBars className={styles.fabar} size={21} />
-          <span className="roboto-medium"> Google Finance</span>
+          <h4 className="roboto-medium">
+            <span style={{ color: "#4285F4" }}>G</span>
+            <span style={{ color: "#EA4335" }}>o</span>
+            <span style={{ color: "#FBBC05" }}>o</span>
+            <span style={{ color: "#34A853" }}>g</span>
+            <span style={{ color: "#EA4335" }}>l</span>
+            <span style={{ color: "#4285F4" }}>e</span>
+            <span className={styles.finance}> Finance</span>
+          </h4>
         </div>
-        <div>
+        <div className={styles.signin}>
           <MdApps className={styles.mdapp} size={26} />
+          <span className="roboto-medium">Sign in</span>
         </div>
       </div>
+      <img src={background} alt="background" className={styles.background} />
+
+      
     </>
   );
 }
